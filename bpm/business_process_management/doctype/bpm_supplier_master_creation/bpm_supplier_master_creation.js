8fc9810495168849b7642a72a3f32ef26c918b1a
dachir <dodziamouzou@gmail.com>
2024-05-05 13:45:06 +0000

    05052024144500

---
+++ bpm/business_process_management/doctype/bpm_supplier_master_creation/bpm_supplier_master_creation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Kossivi and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('BPM Supplier Master Creation', {
+	// refresh: function(frm) {
+
+	// }
+});
